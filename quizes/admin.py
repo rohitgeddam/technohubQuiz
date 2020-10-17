3662c436d45ab42e9092866ee65eb2d7b27aa5f0
rohitgeddam <rohitgeddam2018@gmail.com>
2020-10-17 09:38:57 +0530

    added action to export quiz results

--- quizes/admin.py
+++ quizes/admin.py
@@ -2,7 +2,39 @@ from django.contrib import admin
 import nested_admin
 from django.core.exceptions import ValidationError
 from .models import Question, Quiz, Option, QuizTaker, Response
+
+import decimal, csv
+from django.http import HttpResponse
+
 # Register your models here.
+
+
+# export quiz result to csv action
+def export_quiz_result_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="quiz_result.csv"'
+    writer = csv.writer(response)
+    question_weight = 1
+    for query in queryset:
+        writer.writerow([f'QUIZ NAME', 'TOTAL PARTICIPANTS', 'MAX SCORE ATTAINABLE'])
+        total_number_of_questions = query.questions.all().count()
+        quiz_takers_count = query.quiz_takers.all().count()
+        writer.writerow([ query.name,quiz_takers_count, (total_number_of_questions * question_weight)])
+        writer.writerow([" "])
+        writer.writerow(['USERNAME', 'EMAIL', 'SCORE'])
+        quiz_takers = query.quiz_takers.all().order_by('-score')
+
+        for student in quiz_takers:
+            writer.writerow([student.user.username, student.user.email, student.score])
+    
+        writer.writerow([" "])
+    return response
+
+
+
+
+export_quiz_result_to_csv.short_description = "Export Result To CSV"
+
 
 
 class OptionAdmin(nested_admin.NestedTabularInline):
@@ -20,6 +52,7 @@ class QuizAdmin(nested_admin.NestedModelAdmin):
     prepopulated_fields = {"slug": ('name',),}
     exclude = ['created_by',]
     inlines = [QuestionAdmin]
+    actions = [export_quiz_result_to_csv]
 
     def response_add(self, request, new_object):
         obj = self.after_saving_model_and_related_inlines(new_object)
